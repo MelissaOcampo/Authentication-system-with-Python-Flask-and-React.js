b6d9fe42dfd1d5bef0f4ec42404f21ea53f27243
MelissaOcampo <melissa.ocampo.2001@gmail.com>
2023-02-04 14:18:56 +0000

    Comento el  codigo para mejor comprensión

--- src/app.py
+++ src/app.py
@@ -30,35 +30,41 @@ setup_admin(app)
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 jwt = JWTManager(app)
 
-@app.route("/signup", methods=["POST"])
-def signup():
-    body= json.loads(request.data)
 
-    usuario_signup = Usuario.query.filter_by(email=body["email"]).first()
-    if usuario_signup is None: 
+@app.route("/signup", methods=["POST"]) # Se recibe una solicitud POST en la ruta "/signup"
+def signup():
+    body= json.loads(request.data)   # Se convierte la información enviada en la solicitud a un diccionario llamado body usando json.loads y request.data.
+
+    usuario_signup = Usuario.query.filter_by(email=body["email"]).first() #Se busca en la base de datos si el email enviado en la solicitud ya está registrado usando Usuario.query.filter_by(email=body["email"]).first().
+
+    if usuario_signup is None: #Si no se encuentra ningún usuario con el email enviado, se crea una nueva instancia de la clase Usuario usando los datos enviados en el cuerpo de la solicitud body.
 
         name2 =  Usuario(name=body ["name"], surname=body ["surname"], email=body["email"], password=body["password"]) 
+        
+        #Se agrega la nueva instancia a la base de datos usando db.session.add(name2) y se guardan los cambios en la base de datos usando db.session.commit().
         db.session.add(name2)
         db.session.commit()
         response_body = {"msg":"el usuario fue creado con exito"}
 
-        return jsonify(response_body), 200
+        return jsonify(response_body), 200 #Se devuelve una respuesta con un mensaje de éxito y un código de estado HTTP 200 (OK).
 
 
     response_body = {"msg":"el usuario ya existe en el sistema"}
 
-    return jsonify(response_body), 400
+    return jsonify(response_body), 400 #Si el usuario ya existe en la base de datos, se devuelve una respuesta con un mensaje de error y un código de estado HTTP 400 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["POST"]) #Se recibe una solicitud POST en la ruta "/login"
 def login():
+    #Se extraen los valores de email y password enviados en la solicitud usando request.json.get("email", None) y request.json.get("password", None).
     email = request.json.get("email", None)
     password = request.json.get("password", None)
+    #Se busca en la base de datos si el email enviado en la solicitud ya está registrado usando Usuario.query.filter_by(email=email).first().
     user_login = Usuario.query.filter_by(email=email).first()
     print(user_login)
     if email != user_login.email or password != user_login.password:
-        return jsonify({"msg": "Bad email or password"}), 401
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
+        return jsonify({"msg": "Bad email or password"}), 401 #Si el email o la contraseña enviados no coinciden con los registrados en la base de datos, se devuelve una respuesta con un mensaje de error y un código de estado HTTP 401 
+    access_token = create_access_token(identity=email) 
+    return jsonify(access_token=access_token) #Si el email y la contraseña enviados coinciden con los registrados en la base de datos, se crea un identificador de acceso usando create_access_token y se devuelve una respuesta  y un código de estado HTTP 200 (OK).
 
 # Protect a route with jwt_required, which will kick out requests
 # without a valid JWT present.
